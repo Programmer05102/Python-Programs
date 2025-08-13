cf66a5dceb2c40fba22fbd20e7aaa6ad9b1e9ba3
Programmer05102 <programmer05102@gmail.com>
2025-08-13 21:08:01 +0500

    Simple Arithematic Operations

---
+++ Arithematic_Operator.py
@@ -0,0 +1,19 @@
+
+op = input("Options \n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n Enter Arithematic Opeation You Want To Perform: ")
+f_num = int(input("Enter First Number : "))
+s_num = int(input("Enter Second Number : "))
+
+def A_Operation(a, b):
+    if op == "1":
+        print("The Sum Is: ",a + b)
+    elif op == "2":
+        print("The Differnce Is: ",a - b)
+    elif op == "3":
+        print("The Product Is: ",a * b)
+    elif op == "4":
+        print("The Quotient Is: ", a / b)
+    else:
+        print("Invalid Selection: ")
+
+
+A_Operation(f_num, s_num)
