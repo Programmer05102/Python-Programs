a0d8117769cfa401bf02c1ce8946de0f1f91ae29
Programmer05102 <programmer05102@gmail.com>
2025-08-14 11:40:15 +0500

    Added Percentage Function

--- Arithematic_Operator.py
+++ Arithematic_Operator.py
@@ -1,7 +1,8 @@
 
-op = input("OPTIONS:\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n 5. Modulus\n Enter Arithematic Opeation You Want To Perform: ")
+print("OPTIONS:\n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n 5. Modulus\n 6. Calculate Percentage\n 7. Percentage Of")
 f_num = int(input("Enter First Number : "))
 s_num = int(input("Enter Second Number : "))
+op = input("Enter Arithematic Opeation You Want To Perform: ")
 
 def A_Operation(a, b):
     if op == "1":
@@ -17,5 +18,15 @@ def A_Operation(a, b):
     else:
         print("Invalid Selection: ")
 
+def per(a, b):
+    if op == "6":
+        print("The Percentage Is: ",(a / b) * 100,"%")
+    elif op == "7":
+        print(a,"% of",b,"is",(a / 100) * b)
+    else:
+        print("Invalid Selection: ")
 
-A_Operation(f_num, s_num)
+if op >= "1" and op <= "5":
+    A_Operation(f_num, s_num)
+else:
+    per(f_num, s_num)
